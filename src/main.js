943541bfa5994ee8a86b6e32362d9e7ffc2f8ca5
20000coinbe <kimdks12@gmail.com>
2021-07-06 21:23:34 +0900

    class, prototype

--- src/main.js
+++ src/main.js
@@ -1,14 +1,25 @@
-// @ts-check
+/* eslint-disable */
 
-// test
-const http = require('http')
+// Prototype
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.end('Hello!')
-})
+function Person(name) {
+  this.name = name
+}
 
-const PORT = 3000
-server.listen(PORT, () => {
-  console.log(`The Server is listening at port: ${PORT}`)
-})
+Person.prototype.greet = function greet() {
+  return 'Hi, ${this.name}'
+}
+
+function Student(name) {
+  // 함수를 이용한 클래스 표현
+  this.__prototype__.constructor(name)
+}
+
+Student.prototype.study = function study() {
+  return '${this.name} is studying'
+}
+
+Object.setPrototypeOf(Student.prototype, Person.prototype)
+
+const me = new Student('neo')
+console.log(me.greet())
