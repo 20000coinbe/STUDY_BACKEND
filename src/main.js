97bbcfa06eca69b46db80a347d2ab68b3c52aac5
coinbe <kimdks12@gmail.com>
2021-07-19 17:32:01 +0900

    type def, regex, request

--- src/main.js
+++ src/main.js
@@ -10,6 +10,26 @@
  */
 
 const http = require('http')
+/**
+ * @typedef Post
+ * @property {string} id
+ * @property {string} title
+ * @property {string} content
+ */
+
+/** @type {Post[]} */
+const posts = [
+  {
+    id: 'my_first_post',
+    title: 'My first porst',
+    content: 'Hello',
+  },
+  {
+    id: 'my_second_post',
+    title: 'My second porst',
+    content: 'See ya',
+  },
+]
 
 /**
  * POST
@@ -20,7 +40,41 @@ const http = require('http')
  */
 
 const server = http.createServer((req, res) => {
-  console.log(req.url)
+  const POSTS_ID_REGEX = /^\/posts\/([a-zA-Z0-9-_])+$/
+  const postIdRegexResult =
+    (req.url && POSTS_ID_REGEX.exec(req.url)) || undefined
+
+  if (req.url === '/posts' && req.method === 'GET') {
+    const result = {
+      posts: posts.map((post) => ({
+        id: post.id,
+        title: post.title,
+      })),
+      totalCount: posts.length,
+    }
+
+    res.statusCode = 200
+    res.end('List of posts')
+  } else if (postIdRegexResult) {
+    // GET posts/:id
+    const postId = postIdRegexResult[1]
+    const post = posts.find((_post) => _post.id === postId)
+
+    if (post) {
+      res.setHeader('Content-Type', 'application/json; encoding=utf-8')
+      res.statusCode = 200
+      res.end(JSON.stringify(post))
+    } else {
+      res.statusCode = 404
+      res.end('Post not found')
+    }
+  } else if (req.url === '/posts' && req.method === 'POST') {
+    res.statusCode = 200
+    res.end('Creating post')
+  } else {
+    res.statusCode = 404
+    res.end('Not Found')
+  }
   res.statusCode = 200
   res.end('HELLO')
 })
